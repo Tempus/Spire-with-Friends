6a25f43fc0c3dcc01bedea1d40ffe1cd133f9aa4
Chronometrics <tempus@chronometry.ca>
2019-11-03 01:07:58 -0800

    Abstracted out the playtime to string function

--- src/main/java/chronoMods/ui/hud/VersusTimer.java
+++ src/main/java/chronoMods/ui/hud/VersusTimer.java
@@ -30,27 +30,31 @@ public class VersusTimer {
             if (TogetherManager.gameMode == TogetherManager.mode.Versus) {
                 sb.draw(ImageMaster.TIMER_ICON, Settings.WIDTH - 380f * Settings.scale, Settings.HEIGHT - ((64f) * Settings.scale), 64f * Settings.scale, 64f * Settings.scale);
                 
-                String res = "";
-                float duration = CardCrawlGame.playtime;
-                int seconds = (int)(duration % 60L);
-                int milliseconds = (int)((duration % 1) * 100);
-                duration /= 60L;
-                int minutes = (int)(duration % 60L);
-                int hours = (int)CardCrawlGame.playtime / 3600;
-                if (hours > 0) {
-                  res = String.format("%02d:%02d:%02d:%03d", new Object[] { Integer.valueOf(hours), Integer.valueOf(minutes), Integer.valueOf(seconds), Integer.valueOf(milliseconds) });
-                } else {
-                  res = String.format("%02d:%02d:%03d", new Object[] { Integer.valueOf(minutes), Integer.valueOf(seconds), Integer.valueOf(milliseconds) });
-                }
-
                 FontHelper.renderFontLeftTopAligned(
                     sb,
                     FontHelper.tipBodyFont,
-                    res,
+                    VersusTimer.returnTimeString(CardCrawlGame.playtime),
                     Settings.WIDTH - 320f * Settings.scale,
                     Settings.HEIGHT - (28f) * Settings.scale,
                     Settings.GOLD_COLOR);
             }
         }
+    }
+
+    public static String returnTimeString(float duration) {
+
+        String res = "";
+        int seconds = (int)(duration % 60L);
+        int milliseconds = (int)((duration % 1) * 100);
+        duration /= 60L;
+        int minutes = (int)(duration % 60L);
+        int hours = (int)CardCrawlGame.playtime / 3600;
+        if (hours > 0) {
+          res = String.format("%02d:%02d:%02d:%03d", new Object[] { Integer.valueOf(hours), Integer.valueOf(minutes), Integer.valueOf(seconds), Integer.valueOf(milliseconds) });
+        } else {
+          res = String.format("%02d:%02d:%03d", new Object[] { Integer.valueOf(minutes), Integer.valueOf(seconds), Integer.valueOf(milliseconds) });
+        }
+
+        return res;
     }
 }
