0cafedd6b9f26b6aa1c0e470026327a8315e8440
Chronometrics <tempus@chronometry.ca>
2019-11-03 16:29:10 -0800

    Added comments to Together Manager, and the Lobby field

--- src/main/java/chronoMods/TogetherManager.java
+++ src/main/java/chronoMods/TogetherManager.java
@@ -14,6 +14,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.helpers.FontHelper;
+import com.codedisaster.steamworks.*;
 
 import basemod.*;
 import basemod.abstracts.*;
@@ -45,18 +46,23 @@ public class TogetherManager implements PostDeathSubscriber, PostInitializeSubsc
     public static final Logger logger = LogManager.getLogger(TogetherManager.class.getName());
 
     //This is for the in-game mod settings panel.
-    private static final String MODNAME = "Chronoleague Speedruns";
+    private static final String MODNAME = "Slay the Spire Together";
     private static final String AUTHOR = "Chronometrics";
-    private static final String DESCRIPTION = "Reports speedruns for the chronoleauge.";
+    private static final String DESCRIPTION = "Enables new Coop and Versus Race modes via Steam Networking.";
 
-    public static mode gameMode = TogetherManager.mode.Normal;
-
+    // Stores a list of all the players and the lobby you're connected to
     public static ArrayList<RemotePlayer> players = new ArrayList();
+    public static SteamID currentLobby;
 
+    // Images are stored here because of funky basemod junk, these actually should be loaded in RemotePlayerWidget
     public static Texture panelImg;
     public static ArrayList<Texture> portraitFrames = new ArrayList();
 
+    // Custom UI strings for the mod
     public static Map<String, CustomStrings> CustomStringsMap;
+
+    // Game Mode, just an enum to help mode specific mechanics figure out what's going on
+    public static mode gameMode = TogetherManager.mode.Normal;
 
     public static enum mode
     {
@@ -65,7 +71,7 @@ public class TogetherManager implements PostDeathSubscriber, PostInitializeSubsc
       private mode() {}
     }
 
-    // Constructor, can't do stuff here due to loading reasons
+    // Constructor, can't do stuff here due to the game not being loaded yet.
     public TogetherManager() {
         BaseMod.subscribe(this);
     }
@@ -75,9 +81,10 @@ public class TogetherManager implements PostDeathSubscriber, PostInitializeSubsc
         new TogetherManager();
     }
 
+    // Do stuff here - the game has been safely loaded.
     @Override
     public void receivePostInitialize() {
-        // Load the Mod Badge
+        // Load textures. Why here? Dunno, they only work here.
         Texture badgeTexture = new Texture("chrono/images/Badge.png");
         panelImg = new Texture("chrono/images/playerPanel.png");
         portraitFrames.add(ImageMaster.loadImage("images/ui/relicFrameRare.png"));
@@ -89,17 +96,19 @@ public class TogetherManager implements PostDeathSubscriber, PostInitializeSubsc
         ModPanel settingsPanel = new ModPanel();
         BaseMod.registerModBadge(badgeTexture, MODNAME, AUTHOR, DESCRIPTION, settingsPanel);
 
+        // Initialize the new Steam Networking functions
         NetworkHelper.initialize();
 
         // Custom strings
         CustomStringsMap = CustomStrings.importCustomStrings();
     }
 
+    // Replace this with uploading versus times to the remote leaderboard on my server later
     public void receivePostDeath() {
-        customMetrics metrics = new customMetrics();
+        // customMetrics metrics = new customMetrics();
         
-        Thread t = new Thread(metrics);
-        t.setName("Metrics");
-        t.start();
+        // Thread t = new Thread(metrics);
+        // t.setName("Metrics");
+        // t.start();
     }
 }
